8f33f69d047c9bc6251ac833d8a198a01741a0d3
Carton <bang_liujt@163.ocm>
2019-04-01 20:40:15 +0800

    whether tensorflow run using GPU or not

---
+++ GPU_test.py
@@ -0,0 +1,13 @@
+    # ！/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author:Carton  Time=2019/4/1
+# 测试调用为GPU or CPU
+
+import tensorflow as tf
+
+a = tf.constant([1.,2.,3.,4.,5.,6.], shape=[2,3], name='a')
+b = tf.constant([1.,2.,3.,4.,5.,6.], shape=[3,2], name='b')
+c = tf.matmul(a,b)
+
+with tf.Session(config= tf.ConfigProto(log_device_placement=True)) as sess:
+    print(sess.run(c))
